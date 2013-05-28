61873b7114a33cee1de7c280d59d25f6f5095385
chien vu hoang <fchienvuhoang@gmail.com>
2013-05-29 02:38:31 +0700

    display all vacansy to front end site

---
+++ Source/Share/Public.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Share
+{
+    public class Public
+    {
+        public static string DateVacancysInterviewer;
+    }
+}
